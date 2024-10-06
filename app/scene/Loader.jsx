d6a766db5efae3ae21ec68fdf8239a44e3072efc
Kave McDaniel <kave.mdi@gmail.com>
2024-10-07 01:02:43 +0330

    changed loading in hero

--- app/scene/Loader.jsx
+++ app/scene/Loader.jsx
@@ -2,5 +2,5 @@ import { Html, useProgress } from '@react-three/drei'
 
 export function Loader() {
   const { progress } = useProgress()
-  return <Html center>{progress} % loaded</Html>
+  return <Html center>Loading ... {progress}</Html>
 }
